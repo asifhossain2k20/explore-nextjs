e959692d4f67bfb4e36774f7674a40febedcfee7
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-08-06 15:48:47 +0600

    first commit

---
+++ pages/services/myapp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const myapp = () => {
+    return (
+        <div>
+            <h1>I am My APP</h1>
+        </div>
+    );
+};
+
+export default myapp;
---
+++ pages/user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const user = () => {
+    return (
+        <div>
+            <h1>Hey I am User</h1>
+        </div>
+    );
+};
+
+export default user;
